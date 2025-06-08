b35bfa50f3f3edf2be48f401f066e68a4ded434b
Steven Liang <64478735+YONG-LIN-LIANG@users.noreply.github.com>
2025-06-09 01:05:50 +1000

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,3 +1,9 @@
+/*
+Script Purpose:
+This script creates a new database named 'DataWarehouse' after checking if it already exists.
+If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas within the database: 'bronze' , 'silver', and 'gold'.
+*/
+
 create database datawarehouse;
 create schema if not exists bronze;
 create schema if not exists silver;
