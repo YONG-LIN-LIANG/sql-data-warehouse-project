1a53afb1f7f1cf674bbf9ac9c4e9d0dd8fbc1a33
Steven Liang <64478735+YONG-LIN-LIANG@users.noreply.github.com>
2025-06-15 22:17:17 +1000

    Update btc_create.sql

--- scripts/bronze/crypto/btc_create.sql
+++ scripts/bronze/crypto/btc_create.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS bronze.crypto_btc_trade_history;
-CREATE TABLE if not exists bronze.crypto_btc_trade_history (
+DROP TABLE IF EXISTS bronze.invest_btc_trade_history;
+CREATE TABLE if not exists bronze.invest_btc_trade_history (
     trade_date     DATE,
     price          NUMERIC(10, 2),
     open_price     NUMERIC(10, 2),
